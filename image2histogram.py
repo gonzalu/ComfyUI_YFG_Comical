44a54daf7bcc8b898b5e7f0d03eee67ca85a9d43
Manny Gonzalez <manny@manny.org>
2024-05-07 01:34:42 -0400

    Modified Historgran Node

--- image2histogram.py
+++ image2histogram.py
@@ -9,13 +9,12 @@ import matplotlib.pyplot as plt
 import torch
 
 class ImageHistogramNode:
-    # Define input and output data types
     @classmethod
     def INPUT_TYPES(cls):
         return {"required": {"image": ("IMAGE", {})}}
     
     RETURN_TYPES = ("IMAGE", "IMAGE", "IMAGE", "IMAGE", "IMAGE")
-    RETURN_NAMES = ("Red Channel", "Green Channel", "Blue Channel", "Grayscale Channel", "RGB Histogram")
+    RETURN_NAMES = ("Red Channel", "Green Channel", "Blue Channel", "Luminosity Histogram", "RGB Histogram")
     FUNCTION = "compute_histograms"
     CATEGORY = "YFG"
 
@@ -29,54 +28,48 @@ class ImageHistogramNode:
         image_np = (image_np * 255).astype(np.uint8)
 
         # Calculate histograms for each channel
-        red_hist = np.histogram(image_np[:, :, 0], bins=256, range=(0, 256))[0]
-        green_hist = np.histogram(image_np[:, :, 1], bins=256, range=(0, 256))[0]
-        blue_hist = np.histogram(image_np[:, :, 2], bins=256, range=(0, 256))[0]
+        red_hist = np.histogram(image_np[:, :, 0], bins=256, range=(0, 255))[0]
+        green_hist = np.histogram(image_np[:, :, 1], bins=256, range=(0, 255))[0]
+        blue_hist = np.histogram(image_np[:, :, 2], bins=256, range=(0, 255))[0]
 
-        # Calculate grayscale histogram
-        gray_image = np.dot(image_np[..., :3], [0.2989, 0.5870, 0.1140])
-        gray_hist = np.histogram(gray_image, bins=256, range=(0, 256))[0]
+        # Calculate luminosity histogram
+        luminosity = 0.2989 * image_np[:, :, 0] + 0.5870 * image_np[:, :, 1] + 0.1140 * image_np[:, :, 2]
+        lum_hist = np.histogram(luminosity, bins=256, range=(0, 255))[0]
 
-        # Function to convert histogram to smooth image
         def hist_to_image(hist, color):
             fig, ax = plt.subplots()
-            ax.fill_between(range(256), 0, hist, color=color, step="mid", alpha=0.6)
-            ax.set_facecolor("none")
-            ax.axis('off')  # Remove axes
+            ax.fill_between(range(256), 0, hist, color=color, step='mid', alpha=0.7)
+            ax.set_facecolor("white")
+            ax.axis('off')
             fig.patch.set_visible(False)
             fig.canvas.draw()
+
+            # Convert plot to a numpy array
             data = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
             data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
             plt.close(fig)
-            return torch.tensor(data).permute(2, 0, 1) / 255.0
+            return torch.tensor(data).permute(2, 0, 1).float() / 255
 
-        # Generate images for each channel
+        # Generate images for each channel with respective colors
         red_image = hist_to_image(red_hist, 'red')
         green_image = hist_to_image(green_hist, 'green')
         blue_image = hist_to_image(blue_hist, 'blue')
-        gray_image = hist_to_image(gray_hist, 'gray')
+        lum_image = hist_to_image(lum_hist, 'gray')
 
-        # Create RGB Histogram by overlaying each channel histogram
-        fig, ax = plt.subplots()
-        ax.fill_between(range(256), 0, red_hist, color='red', alpha=0.6)
-        ax.fill_between(range(256), 0, green_hist, color='green', alpha=0.6)
-        ax.fill_between(range(256), 0, blue_hist, color='blue', alpha=0.6)
-        ax.set_facecolor("none")
-        ax.axis('off')  # Remove axes
-        fig.patch.set_visible(False)
-        fig.canvas.draw()
-        rgb_data = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
-        rgb_data = rgb_data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
-        plt.close(fig)
-        rgb_image = torch.tensor(rgb_data).permute(2, 0, 1) / 255.0
+        # Create RGB Histogram by overlaying each color histogram
+        composite_fig, composite_ax = plt.subplots()
+        for hist, color in zip([red_hist, green_hist, blue_hist], ['red', 'green', 'blue']):
+            composite_ax.fill_between(range(256), 0, hist, color=color, alpha=0.5, step='mid')
+        composite_ax.set_facecolor("white")
+        composite_ax.axis('off')
+        composite_fig.patch.set_visible(False)
+        composite_fig.canvas.draw()
 
-        return red_image, green_image, blue_image, gray_image, rgb_image
+        composite_data = np.frombuffer(composite_fig.canvas.tostring_rgb(), dtype=np.uint8)
+        composite_data = composite_data.reshape(composite_fig.canvas.get_width_height()[::-1] + (3,))
+        plt.close(composite_fig)
+        rgb_image = torch.tensor(composite_data).permute(2, 0, 1).float() / 255
 
+        return red_image, green_image, blue_image, lum_image, rgb_image
 
-# NODE_CLASS_MAPPINGS = {
-    # "image_histogram_node": ImageHistogramNode
-# }
-
-# NODE_DISPLAY_NAME_MAPPINGS = {
-    # "image_histogram_node": "Image Histogram Generator"
-# }
+# No additional configuration needed here since class and display mappings are handled separately.
