76fc0ddc1e8a031801804314c6742d0330b8e802
Manny Gonzalez <manny@manny.org>
2024-06-09 02:26:22 -0400

    Remove Node

--- __init__.py
+++ __init__.py
@@ -3,7 +3,6 @@ from .image2histogramscompact import ImageHistogramsNodeCompact
 from .image2halftone import ImageHalftoneNode
 from .images2sidebyside import ImagesSideBySideNode
 from .switchers.imageSwitchers import *
-from .image2Base64node import ImageToBase64Node
 from .loaders.image2imgbb import Image2ImgBB
 from .loaders.imgbbLoader import *
 from .loaders.smartcheckpointloader import SmartCheckpointLoader
@@ -22,7 +21,6 @@ NODE_CLASS_MAPPINGS = {
     "Image10SwitcherNode": Image10SwitcherNode,
     "Image15SwitcherNode": Image15SwitcherNode,
     "Image20SwitcherNode": Image20SwitcherNode,
-    "image_to_base64_node": ImageToBase64Node,
     "image2imbgg_node": Image2ImgBB,    
     "imgbbLoader_node": ImgbbLoader,
     "storeURL_node": storeURL,
@@ -43,7 +41,6 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "Image10SwitcherNode": "\ud83d\udc2f YFG 10 Image Switcher",
     "Image15SwitcherNode": "\ud83d\udc2f YFG 15 Image Switcher",
     "Image20SwitcherNode": "\ud83d\udc2f YFG 20 Image Switcher",
-    "image_to_base64_node": "\ud83d\udc2f YFG Image to Base64",
     "image2imbgg_node": "\ud83d\udc2f YFG Image to imgBB",
     "imgbbLoader_node": "\ud83d\udc2f YFG imgBB to Image",
     "storeURL_node": "\ud83d\udc2f YFG Store URL",    
@@ -53,4 +50,5 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "MonoClip_node": "\ud83d\udc2f YFG Monochrome Image Clip",
     "PixelArt_node": "\ud83d\udc2f YFG PixelArt",
 }
+WEB_DIRECTORY = "./web"
 print(f"\033[1;32m[YFG Comical]: 🐯 \033[93m\033[3m Loaded {len(NODE_CLASS_MAPPINGS)} nodes.\033[0m 🐯")
